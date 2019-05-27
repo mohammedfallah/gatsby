39164f1805da0b5f395466852ba943653b12e63e
mohammedfallah <mohammedfallah838@gmail.com>
2019-05-27 18:08:10 +0430

    navbar added

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,9 +4,12 @@ import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 import '../components/bootstrap.min.css'
 import "./layout.css"
-
+import Navbar from '../components/Globals/Navbar'
 const Layout = ({ children }) => (
- <>{children}</>
+ <>
+ <Navbar />
+ {children}</>
+ 
 )
 
 Layout.propTypes = {
